a31cfaeb609106551ebea143878226c547cd96b6
Vaidehi Shah <143230447+vaidehi-nshah@users.noreply.github.com>
2023-11-04 01:16:07 -0400

    idk

--- script.js
+++ script.js
@@ -79,6 +79,26 @@ workBtn.addEventListener("click", () => {
   portfolio.style.display = "none";
 });
 
+// // Click events for buttons
+// const portfolio = document.getElementById("portfolio");
+// const portfolioBtn = document.getElementById("portfolio-btn");
+// const skills = document.getElementById("skills");
+// const skillsBtn = document.getElementById("skills-btn");
+
+// portfolioBtn.addEventListener("click", (event) => {
+//   skills.style.display = "none";
+//   portfolio.style.display = "flex";
+//   skillsBtn.classList.remove("active-btn");
+//   portfolioBtn.classList.add("active-btn");
+// });
+
+// skillsBtn.addEventListener("click", (event) => {
+//   skills.style.display = "flex";
+//   portfolio.style.display = "none";
+//   portfolioBtn.classList.remove("active-btn");
+//   skillsBtn.classList.add("active-btn");
+// });
+
 function changeImage(imageId, newImagePath) {
   const image = document.getElementById(imageId);
   
